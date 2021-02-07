1b14d153a8db4fec8b2a252a0066911c1280beb6
Mathias Quintero <me@quintero.io>
2021-02-07 15:14:01 +0100

    updating syntax

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
                  targets: ["MarkdownSyntax"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nerdsupremacist/Syntax.git", from: "1.0.0"),
+        .package(url: "https://github.com/nerdsupremacist/Syntax.git", from: "1.0.1"),
     ],
     targets: [
         .target(name: "MarkdownSyntax",
