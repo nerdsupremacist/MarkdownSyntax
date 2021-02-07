2bba299cfbe169bdd5edccf19181642e682c2c01
Mathias Quintero <me@quintero.io>
2021-02-07 15:14:15 +0100

    Allowing for html nodes without any children

--- Tests/MarkdownSyntaxTests/MarkdownSyntaxTests.swift
+++ Tests/MarkdownSyntaxTests/MarkdownSyntaxTests.swift
@@ -4,9 +4,19 @@ import XCTest
 final class MarkdownSyntaxTests: XCTestCase {
     func testExample() throws {
         let code = """
-        <div class="center">
-            Hello world!
-        </div>
+        <p align="center">
+            <img src="logo.png" width="400" max-width="90%" alt="Syntax" />
+        </p>
+
+        <p align="center">
+            <img src="https://img.shields.io/badge/Swift-5.3-orange.svg" />
+            <a href="https://swift.org/package-manager">
+                <img src="https://img.shields.io/badge/swiftpm-compatible-brightgreen.svg?style=flat" alt="Swift Package Manager" />
+            </a>
+            <a href="https://twitter.com/nerdsupremacist">
+                <img src="https://img.shields.io/badge/twitter-@nerdsupremacist-blue.svg?style=flat" alt="Twitter: @nerdsupremacist" />
+            </a>
+        </p>
 
         # Hello World
         ## This is another heading
